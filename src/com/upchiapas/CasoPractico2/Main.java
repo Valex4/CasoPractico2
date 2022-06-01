ffc71e0970783b445befe191a9d00d2a355080cc
YahirAlexander <yam77123@hotmail>
2022-05-31 23:02:09 -0500

    sexto commit, ya quedo solo implementare la parte de un do while al inicio solo para que me deje menter 8 participantes

--- src/com/upchiapas/CasoPractico2/Main.java
+++ src/com/upchiapas/CasoPractico2/Main.java
@@ -7,16 +7,18 @@ import java.util.Scanner;
 public class Main {
     private static Scanner entrada = new Scanner(System.in);
 
-    static ArrayList<Participante> listaParticipante = new ArrayList<Participante>();
+    static ArrayList<Participante> listaParticipante = new ArrayList<Participante>(7);
+
     public static void main(String[] args) {
         Main listaP = new Main();
         listaP.listaParticipante = new ArrayList();
         leerOpcion();
     }
 
-    public static void leerOpcion(){
+    public static void leerOpcion() {
         int opcion = 0;
-        int posArray = 0;
+        int posArray = 1;
+        int posArrat = 0;
         do {
             System.out.println("Seleccione la opcion deseada: ");
             System.out.println("[1] Agregar participantes ");
@@ -24,21 +26,23 @@ public class Main {
             System.out.println("[3] Salir\n");
             System.out.println("Ingrese la opcion del (1-3)");
             opcion = entrada.nextInt();
-            switch (opcion){
+            switch (opcion) {
                 case 1:
                     ingresarValores(posArray);
                     posArray++;
                     break;
                 case 2:
-                    visualisarP();
+                    visualisarP(posArrat);
+                    posArrat++;
                     break;
             }
-        }while (opcion !=3);
+        } while (opcion != 3);
     }
-    public static void ingresarValores(int posArray){
+
+    public static void ingresarValores(int posArray) {
         int folio = 0;
-        Random r= new Random();
-        folio=(int)((Math.random() * (999 - 100)) + 100); //poner esto dentro de un metodo para luego poderlo compararlo con un if para ver que no se repitan
+        Random r = new Random();
+        folio = (int) ((Math.random() * (999 - 100)) + 100);
         String nombre;
         String lugarRepresenta;
         System.out.println("Su numero de folio es: ");
@@ -54,17 +58,20 @@ public class Main {
         listaParticipante.add(participante);
     }
 
-    public static void visualisarP(){
-        for (int i=0; i < listaParticipante.size(); i++){
-            System.out.println(listaParticipante.get(i).toString());
+    public static void visualisarP(int posArrat) {
+        int plantaBaja = 0;
+        for (int i = 0; i < listaParticipante.size(); i++) {
+
+            posArrat++;
+            if (posArrat < 6) {
+                System.out.println(listaParticipante.get(i).toString() + " su posicion es: Planta alta [" + posArrat + "]");
+            } else if (posArrat > 5 && posArrat <9) {
+                plantaBaja++;
+                System.out.println(listaParticipante.get(i).toString() + " su posicion es: Planta baja [" + plantaBaja + "]");
+            }
         }
-    }
-    public int generarNumero(){
-        int folio = 0;
-        Random r= new Random();
-        folio=(int)((Math.random() * (999 - 100)) + 100); //poner esto dentro de un metodo para luego poderlo compararlo con un if para ver que no se repitan
-        return folio;
-    }
+        }
 
 
-}
+    }
+
